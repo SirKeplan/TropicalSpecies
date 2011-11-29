3663d0f09e7e1dba9ff1c6312389a2b04b52f7d0
RichardMorris <rich@singsurf.org>
2011-11-29 15:23:23 +0000

    Closed paragraph

--- footer.php
+++ footer.php
@@ -18,3 +18,4 @@ $result = mysql_query("SELECT * FROM `tropicalspecies`");
 <br />   
    The database and code is licensed under a 
    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+</p>
