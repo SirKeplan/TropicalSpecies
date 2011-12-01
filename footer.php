ba9d4593777d9c1f1c4a64c266ab09f2e0db548e
RichardMorris <rich@singsurf.org>
2011-12-01 14:08:21 +0000

    SELECT count(*) FROM `tropicalspecies` is a faster way to get the number of rows in a table.

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 </div>
 <?php
-$result = mysql_query("SELECT * FROM `tropicalspecies`"); 
-	echo "<p class=\"small\"><b>Last update on 29/11/11:</b> Now containing ".mysql_num_rows($result)." plants.</p>\n	";
+$numPlantsRes = mysql_query("SELECT count(*) FROM `tropicalspecies`");
+$numPlantsRow = mysql_fetch_row($result2);
+$numPlants = $row[0];
+echo "<p class=\"small\"><b>Last update on 29/11/11:</b> Now containing $numPlants plants.</p>\n";
 
 
 ?>
