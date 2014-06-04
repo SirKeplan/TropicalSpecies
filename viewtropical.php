e45c4e2f14d80151b13863cc53c3f3df39e6153e
Ajna Fern <ajna@theferns.info>
2014-06-04 20:14:38 +0100

    remove unused code and comments

--- viewtropical.php
+++ viewtropical.php
@@ -7,24 +7,15 @@
   <link rel="shortcut icon" href="flower.ico">
 
 <?php
-	function callback($matches) {
-		
-		#echo "|";
-		#print_r( $matches);
-		#echo "|";
-		
+	function callback($matches) {		
 		$key = $matches[1];
-		
 		if ($key == "K") {
 			$row2 = array("No" => "K", "Title" => "Plants for a Future", "Author" => "Ken Fern ", "Description" => "Notes from observations, tasting etc at Plants For A Future and on field trips.");
 
-			#mysql_close($db);
-			#return;
 		} else {
 			if ($key) {
 				$result2 = safe_query("SELECT * FROM `References` WHERE `No` = ".$key);
 				$row2 = mysql_fetch_assoc($result2);
-				#echo $row2["Title"];
 			} else {
 				return $key;
 			}
@@ -33,15 +24,10 @@
 		if ($row2) {
 			$out = "";
 			$out = '<div class="ref"><a href="refs.php#'.$key.'">'.$key.'</a><div>'.OutputBookRefRecord($row2).'</div></div>';
-			
-			#$out .= OutputBookRefRecord($row2);
-			#$out .= 
-		}
-		#echo "function:";
-		#echo htmlspecialchars ($out);
-		return $out;//$matches[0];//$matches[0];
-	}
 
+		}
+		return $out;
+	}
 
 	function link_to_book($string, $other = false) {
 		if ($other){			
@@ -53,87 +39,10 @@
 
 		$rep = array();
 		$pat = array();
-		#$out = "";
-		/*
-		for ($i = 0; $i < count($matches[1]); $i++) {
-			$pat[$i] = $regex;#"/{$matches[$i]}/";
-			$rep[$i] = '<a href="bookref.php?id=$1" onClick="RefWindow=window.open(\'bookref.php?id=$1\',\'RefWindow\',\'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=400,height=610,left=50,top=150\'); RefWindow.focus(); return false;">$1</a>';
-			
-			$key = $matches[1][$i];
-			$row2 = null;
-			$row2 = array("No" => $key, "Title" => "Plants for a Future", "Author" => "Ken Fern ", "Description" => "Notes from observations, tasting etc at Plants For A Future and on field trips.");
 
-			
-			
-			if ($key == "K") {
-				$row2 = array("No" => "K", "Title" => "Plants for a Future", "Author" => "Ken Fern ", "Description" => "Notes from observations, tasting etc at Plants For A Future and on field trips.");
-
-				#mysql_close($db);
-				#return;
-			} else {
-				if ($key) {
-					$result2 = safe_query("SELECT * FROM `References` WHERE `No` = ".$key);
-					$row2 = mysql_fetch_assoc($result2);
-					#echo $row2["Title"];
-				}
-			}
-			
-			if ($row2) {
-				$rep[$i] = "";
-				$rep[$i] .= '<span class="ref"><a href="#">$1</a><span>';
-				$rep[$i] .= OutputBookRefRecord($row2);
-				$rep[$i] .= '</span></span>';
-				#$rep[$i] = $out;
-			}
-			#echo "INDEX~~".$i."</br>";
-
-			
-		}
-		*/
-		#print_r ($rep);
 		$newstring = preg_replace_callback($regex, "callback", $string);
-		#$newstring = preg_replace($pat, $rep, $string);
-		#$newstring = str_replace($matches[1], $rep, $string);
+
 		return $newstring;
-	}
-
-
-	function OutputRecord($row) {
-		#$order = array( "Latin name","Common name","Author",
-		#				"Botanical references","Family","Range",
-		#				"Habitat","Known hazards","Cultivation details",
-		#				"Edible uses","Uses notes","Medicinal",
-		#				"Propagation 1","WeedPotential",
-		#				"ConservationStatus","AgroforestryUses",
-		#				"NomenclatureNotes","GeneralInformation");
-		#echo "<p>";
-		#foreach ($order as $col) {
-		#	print_r("<h3>".$col.":</h3>");
-		#	print_r($row1[$col]."<br>\n\n");
-		#}
-		
-		#######
-		echo link_to_book($row['Cultivation details']);
-		
-		include ('template.php');
-
-
-		######
-		
-		
-		#$result = safe_query("DESCRIBE `tropicalspecies`");
-		#if (!$result) {
-		#    echo 'Could not run query: ' . mysql_error();
-		#    exit;
-		#}
-		#if (mysql_num_rows($result) > 0) {
-		#    while ($row = mysql_fetch_row($result)) {
-		#	$col_name = $row[0];
-		#	print_r("<h3>".$col_name.":</h3>");
-		#	print_r($row1[$col_name]."<br>\n\n");
-		#    }
-		#}}
-		#echo "</p>";
 	}
 	
 	include 'functions.php';
@@ -143,31 +52,19 @@
 		trigger_error("A plant name must be supplied.");
 	}
 	$key = mysql_real_escape_string($_GET["id"]);
-	#include 'dbconnect.php';
-	/*
-	SELECT *
-	FROM `TropicalSpecies`
-	WHERE `Latin name` = 'Abelmoschus moschatus'
-	*/
 
 	$result = safe_query("SELECT * FROM `tropicalspecies` WHERE LCASE(`Latin name`) = LCASE('$key')");
 	
-	//echo "<p>";
-	//echo "<table border = \"1\" >";
-	//echo "<th>Latin Name</th><th>Commom Name</th><th></th>";
-
 	$row = mysql_fetch_assoc($result);
 	if ($row) {
 		echo "<title>".$row['Latin name']." - Useful Tropical Plants</title>";
 		echo "</head>\n<body>";	
 		include 'header.php';
-			#echo link_to_book($row['Cultivation details']);
+
 		global $row;
 		include('template.php' );
 		echo "<script src=\"boxmove.js\"></script>";
-		#include('commentsform.php' );
 		
-		#OutputRecord($row);
 	} else {
 		echo "<title>".$key."</title>";
 		echo "</head>\n<body>";
