cf6bd432ecfdf5d459940b1775d992acf9c8239d
Ajna Fern <ajna@theferns.info>
2014-06-26 18:10:50 +0100

    add error page

---
+++ notfound.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel=stylesheet href="style.css" type="text/css">
+  <link rel="shortcut icon" href="flower.ico">
+  <title>Page Not Found - Useful Tropical Plants</title>
+</head>
+<body>
+<?php include 'header.php' ?>
+	<h1>Page Not Found</h1>
+	<p>404 - Page not found, Go <a href="javascript:history.back();">Back</a>
+	 or try the <a href="./">Home Page</a>.</p>
+<?php 
+#trigger_error("404 - Page Not Found.");
+include 'footer.php' ?>
