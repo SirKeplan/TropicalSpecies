a9e0d61e60dfa7b133f28b075164777af05eb078
Ajna Fern <ajna@theferns.info>
2014-07-08 12:18:59 +0100

    tweak rdf in footer(am i doing it right?)

--- footer.php
+++ footer.php
@@ -14,15 +14,15 @@ $words = safe_query("SHOW TABLE STATUS LIKE 'tropicalspecies'");
 $row = mysql_fetch_row($words);
  $date =  date('Y-m-d', strtotime($row[11]));
 
-echo "</div><div class=\"FOOTER\"><br/><p class=\"small\"><b>Last update on {$date}:</b> Now containing $numPlants plants.</p>\n";
-
+echo '</div><div class="FOOTER" prefix="dct: http://purl.org/dc/terms/ cc: http://creativecommons.org/ns#"><br/><p class="small"><b>Last update on '.$date.':</b> Now containing $numPlants plants.</p>';
 
 ?>
+
 <p class="small">
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
+<a rel="cc:license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
 	<img alt="Creative Commons License" class="cclicense" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" />
 </a>
-<span prefix="dct: http://purl.org/dc/terms/ cc: http://creativecommons.org/ns#" href="http://purl.org/dc/dcmitype/Dataset" property="dct:title" rel="dct:type">Tropical Species Database</span> 2012 by 
+<span href="http://purl.org/dc/dcmitype/Dataset" property="dct:title" rel="dct:type">Useful Tropical Plants Database</span> 2012 by 
 	<a href="http://tropical.theferns.info/" property="cc:attributionName" rel="cc:attributionURL">Ken Fern</a>, 
 	web interface by 
 	<a href="http://ajna.theferns.info/" property="cc:attributionName" rel="cc:attributionURL">Ajna Fern</a>
@@ -30,6 +30,6 @@ echo "</div><div class=\"FOOTER\"><br/><p class=\"small\"><b>Last update on {$da
 	<a href="http://singsurf.org/" property="cc:attributionName" rel="cc:attributionURL">Richard Morris</a>.
 	<br />   
 	The database and code is licensed under a 
-	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+	<a rel="cc:license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
 </p>
 </div>
